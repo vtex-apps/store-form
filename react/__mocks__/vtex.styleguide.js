d1b52683e82a32bd3f75b5799cb28d2f67242ee4
Helena Steck <helena.steck@vtex.com.br>
2020-01-31 18:01:35 -0300

    Create test files

---
+++ react/__mocks__/vtex.styleguide.js
@@ -0,0 +1,8 @@
+import Input from '@vtex/styleguide/lib/Input'
+import Checkbox from '@vtex/styleguide/lib/Checkbox'
+import CheckboxGroup from '@vtex/styleguide/lib/CheckboxGroup'
+import RadioGroup from '@vtex/styleguide/lib/RadioGroup'
+import Dropdown from '@vtex/styleguide/lib/Dropdown'
+import Textarea from '@vtex/styleguide/lib/Textarea'
+
+export { Input, Checkbox, CheckboxGroup, RadioGroup, Dropdown, Textarea }
