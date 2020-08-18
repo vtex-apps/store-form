b077e309a39ae651033262e8acf629e0cd06aa70
doruradu <doru@iviteb.com>
2020-08-18 23:06:54 +0300

    Fix textarea label

--- react/components/TextArea.tsx
+++ react/components/TextArea.tsx
@@ -1,24 +1,28 @@
 import React, { FC } from 'react'
+import { BaseInputProps } from '../typings/InputProps'
 import { Textarea as StyleguideTextarea } from 'vtex.styleguide'
 import { UseTextAreaReturnType, useTextArea } from 'react-hook-form-jsonschema'
 
 import { useFormattedError } from '../hooks/useErrorMessage'
 
-export const TextAreaInput: FC<{ pointer: string }> = props => {
+export const TextAreaInput: FC<BaseInputProps> = props => {
   const textAreaObject = useTextArea(props.pointer)
-  return <TextArea textAreaObject={textAreaObject} />
+  return <TextArea textAreaObject={textAreaObject} label={props.label} />
 }
 
 export const TextArea: FC<{
-  textAreaObject: UseTextAreaReturnType
+  textAreaObject: UseTextAreaReturnType,
+  label?: string
 }> = props => {
   const { textAreaObject } = props
   const error = textAreaObject.getError()
+  const subSchema = textAreaObject.getObject()
+  const label = props.label ?? subSchema.title ?? textAreaObject.name
 
   return (
     <StyleguideTextarea
       {...textAreaObject.getTextAreaProps()}
-      label={textAreaObject.getObject().title}
+      label={label}
       error={!!error}
       errorMessage={useFormattedError(error)}
     />
