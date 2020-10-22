bbfffe5cd3e1ee8b3e8a36d4f73ada547ce6bc98
Breno Calazans <brenocalazans@gmail.com>
2020-10-22 16:54:11 -0300

    Update eslint and ts config

--- react/FormInputText.tsx
+++ react/FormInputText.tsx
@@ -4,15 +4,15 @@ import { RawInput, HiddenInput, PasswordInput } from './components/Input'
 import { FormRawInputProps, InputTypes } from './typings/InputProps'
 
 export default function FormInput(props: FormRawInputProps) {
-  const { inputType = InputTypes.input, ...rest } = props
+  const { inputType = InputTypes.input, pointer, ...rest } = props
 
   // eslint-disable-next-line default-case
   switch (inputType) {
     case InputTypes.input:
-      return <RawInput pointer={props.pointer} {...rest} />
+      return <RawInput pointer={pointer} {...rest} />
     case InputTypes.hidden:
-      return <HiddenInput pointer={props.pointer} {...rest} />
+      return <HiddenInput pointer={pointer} {...rest} />
     case InputTypes.password:
-      return <PasswordInput pointer={props.pointer} {...rest} />
+      return <PasswordInput pointer={pointer} {...rest} />
   }
 }
